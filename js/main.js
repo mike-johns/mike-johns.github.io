2f54b4e27beb52def08d1af9edb7e3d598a65788
Mike Johns <hello@mikejohns.email>
2016-03-16 18:08:43 -0700

    Fixed JS Test
    Corrected test selector in <main.js>

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 
 // Adding a quick function to add a class to a header object, just to test with.
 
-$('h1:first').on(click, function(e) {
-    e.target.addClass('is-blue');
+$('h1').on('click', function(e) {
+    var $this = $(e.target);
+    $this.addClass('is-blue');
 })
