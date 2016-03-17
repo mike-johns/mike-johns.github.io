05e3b17dfeaa01ed5ac4dba37397240c089fee1a
Mike Johns <hello@mikejohns.email>
2016-03-16 18:43:54 -0700

    More Building Updates
    Added form field in <index.html> and corresponding classes in
    <main.css>. These mostly came from a previous project that I wrote. I
    also added some code in <main.js> that appends a new text submission to
    an empty <p> object under the form.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-
-
 // Adding a quick function to add a class to a header object, just to test with.
 
 $('h1').on('click', function(e) {
@@ -9,4 +7,18 @@ $('h1').on('click', function(e) {
     } else {
         $this.addClass('is-blue');
     }
+})
+
+// Assign jQuery selector to the form field where the user will submit new text
+
+var $newTextField = $('#new-text-field');
+
+// TODO: Get text of newly edited form field and update a header with that text
+
+$('#submit-button').on('click', function() {
+    var newText = $newTextField.val();
+    if (newText) {
+        $('#user-text-display').append(newText);
+    }
+    $newTextField.val('');
 })
