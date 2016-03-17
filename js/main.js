5ec6b3f8dd0d5d7b06bc1f9a9824c06bf581f3e0
Mike Johns <hello@mikejohns.email>
2016-03-16 19:06:21 -0700

    Added More JS Functionality
    Continuing to expand the functionality of <main.js> and the elements to
    support it in <index.html>. Specifically in this commit, the storage of
    all messages the user has submitted in an Array that is displayed under
    the most current message. Also added some semicolons throughout at the
    end of jQuery functions.

--- js/main.js
+++ js/main.js
@@ -7,18 +7,40 @@ $('h1').on('click', function(e) {
     } else {
         $this.addClass('is-blue');
     }
-})
+});
 
 // Assign jQuery selector to the form field where the user will submit new text
 
 var $newTextField = $('#new-text-field');
 
-// TODO: Get text of newly edited form field and update a header with that text
+// Assign jQuery selector to the <p> element that will display the user's most recent message
+
+var $recentMessage = $('#user-text-display');
+
+// Assign jQuery selector to the <p> element that will display the user's message history
+
+var $messageHistoryDisplay = $('#user-history-display');
+
+// Assign a variable to hold an Array of the user's submitted text strings.
+
+var userTextArray = [];
+
+// Get text of newly edited form field and update a header with that text
 
 $('#submit-button').on('click', function() {
     var newText = $newTextField.val();
     if (newText) {
-        $('#user-text-display').append(newText);
+        $recentMessage.html(function() {
+            return '<strong>Latest Message: </strong>' + newText
+        });
+        var itemCount = userTextArray.push(newText);
+        var newItemIndex = (itemCount - 1);
+        $messageHistoryDisplay.html(function() {
+            return '<strong>Stored Messages: </strong>' + userTextArray;
+        });
     }
     $newTextField.val('');
-})
+});
+
+// Replicate above function, which appends user-submitted text to a selected <p> object, so that the text also updates when the form is submitted.
+
