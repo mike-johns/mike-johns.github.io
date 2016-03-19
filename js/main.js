04a6e27e2a12b294bbd5fe1c4557c9e6e18d4892
Mike Johns <hello@mikejohns.email>
2016-03-19 00:57:08 -0700

    Major UI Improvements
    Added several CSS classes to style the UI. Also added a method and
    animation to create the focus update notifier. I’m tired.

--- js/main.js
+++ js/main.js
@@ -20,6 +20,8 @@ var $availableDisplay = $('#available-display');
 
 var $guestSelection = $('#guest-selection');
 
+var $focusUpdate = $('#focus-update-large');
+
 var $log = $('#log-message');
 
 // Fill in the text or html content of a few of these elements selected above.
@@ -34,10 +36,10 @@ $availableDisplay.html(function() {
 
 // Declare a function to update the #log-message text element and log the message to the JS console.
 
+// TODO: fix this animation in the case that multiple messages get updated quickly. Also, why does the first one not fade in?
+
 function updateAndLog(message) {
-    $log.html(function(){
-       return '<strong>Log: </strong>' + message 
-    });
+    $log.text(message).fadeIn(500).delay(3000).fadeOut(1000);
     console.log(message);
 }
 
@@ -52,6 +54,7 @@ function checkIn(name) {
                 $guestSelection.append('<option>' + name + '</option>');
                 availability--;
                 updateAndLog(name + ' has been admitted as Guest #' + guestPosition + '. There are ' + availability + ' more spots available.');
+                focusUpdate(name, ' has been checked in');
             } else {
                 for (var i = 0; i < attendantList.length; i++) {
                     if (attendantList[i] == undefined) {
@@ -59,6 +62,7 @@ function checkIn(name) {
                         $guestSelection.append('<option>' + name + '</option>');
                         availability--;
                         updateAndLog(name + ' has been admitted as guest #' + (i + 1) + '. There are ' + availability + ' more spots available.');
+                        focusUpdate(name, ' has been checked in');
                         return;
                     }
                 }
@@ -66,6 +70,7 @@ function checkIn(name) {
                 $guestSelection.append('<option>' + name + '</option>');
                 availability--;
                 updateAndLog(name + ' has been admitted as guest #' + (attendantList.length) + '. There are ' + availability + ' more spots available.');
+                focusUpdate(name, ' has been checked in');
             }
         } else {
             alert('There is no room available for ' + name + '.');
@@ -85,6 +90,7 @@ function checkOut(guestName) {
                 }
                 $('option:contains(' + guestName +')').remove();
                 updateAndLog(guestName + ' has been checked out.');
+                focusUpdate(guestName, ' has been checked out');
             }
         }
     }
@@ -115,3 +121,13 @@ $('#guest-checkout-form').on('submit', function(e) {
        return '<strong>Available: </strong>' + availability;
     });
 });
+
+// Animations and functions for top-level activity display
+
+$focusUpdate.hide();
+
+function focusUpdate(gName, gMessage) {
+    $focusUpdate.text('');
+    $focusUpdate.append(gName +'<small> ' + gMessage + '</small>');
+    $focusUpdate.fadeIn(500).delay(2000).fadeOut(750);
+}
